424ff323ffa25fdb223cb199475f35396c4e1635
Taffyw <15709287714@163.com>
2017-06-22 16:30:12 +0800

    歌手详情页添加

---
+++ src/components/singer/SingerDetail.vue
@@ -0,0 +1,42 @@
+<template>
+  <transition name="slide">
+    <div class="singer-detail">
+      <h2>我是歌手详情页面</h2>
+      <h4>{{name}}</h4>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: 'singerdetail',
+    data () {
+      return {
+        name: this.$route.query.name
+      }
+    },
+    mounted() {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @import "~sass/var";
+  .singer-detail{
+    z-index: 101;
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: $bg-color;
+  }
+  .slide-enter-active, .slide-leave-active{
+    transition: all 0.3s
+  }
+  .slide-enter, .slide-leave-to{
+    transform: translate3d(100%, 0, 0)
+  }
+</style>
