ea22b33852fbe9cc08b8857296ff44f5d91e8857
Taffyw <15709287714@163.com>
2017-06-27 17:01:25 +0800

    add新方法

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -6,3 +6,10 @@
  *
  */
 export const SET_SINGER = 'SET_SINGER'
+
+export const SET_PLAY_STATE = 'SET_PLAY_STATE'
+export const SET_FULL_SCREEN = 'SET_FULL_SCREEN'
+export const SET_PLAY_LIST = 'SET_PLAY_LIST'
+export const SET_SUFX_LIST = 'SET_SUFX_LIST'
+export const SET_PLAY_MODE = 'SET_PLAY_MODE'
+export const SET_PLAY_INDEX = 'SET_PLAY_INDEX'
